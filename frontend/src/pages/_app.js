30c015b82fba9ba011285a5616f759ebc11acc41
WinayGupta777 <vinay.gupta8101@gmail.com>
2023-04-07 16:59:09 +0530

    Integrated App with MaterialUI; Added packages for mui; Support given for dark & light theme; Dark mode applied;

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import { Paper, ThemeProvider } from "@mui/material";
+import { darkTheme } from "../utils/theme";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <ThemeProvider theme={darkTheme}>
+    <Paper style={{ height: "100vh" }} square={true}>
+    <Component {...pageProps} />
+    </Paper>
+  </ThemeProvider>
+  )
 }
