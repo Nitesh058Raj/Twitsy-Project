c5903af77c1aa41c66187b7322bd3656bf8378ba
Nitesh <05niteshraj@gmail.com>
2023-04-13 15:37:27 +0530

    Added colors-varibles to store color related data in diff file;

---
+++ frontend/src/styles/colors-variables.js
@@ -0,0 +1,16 @@
+export const COLORS = {
+  text: 'white',
+  text_gray: 'gray',
+  primary: '#61ffe5',
+  primary_hover: '#00e4f5',
+  background_dark: '#041f19',
+  background_light: '#149a7d',
+
+  black: "#000",
+  white: "#fff",
+  main_primary: "#66d9e8",
+
+  main_secondary: "#0b7285",
+  main_light_secondary: "#0c8599",
+  main_light_secondary_2: "#1098ad",
+}
